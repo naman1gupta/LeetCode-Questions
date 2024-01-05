6021bc57f9888f3633a754e9dd211a0d7f5b39ec
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2024-01-05 21:40:54 +0530

    Time: 1484 ms (94.27%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,21 +1,17 @@
-WITH
-  cte_first_year AS (
-    SELECT
-      product_id,
-      year,
-      quantity,
-      price,
-      RANK() OVER (PARTITION BY product_id ORDER BY year) AS year_number
-    FROM
-      Sales
-  )
-
-SELECT
-  product_id,
-  year AS first_year,
-  quantity,
-  price
-FROM
-  cte_first_year
-WHERE
-  year_number = 1
+SELECT 
+  product_id, 
+  year AS first_year, 
+  quantity, 
+  price 
+FROM 
+  Sales 
+WHERE 
+  (product_id, year) IN (
+    SELECT 
+      product_id, 
+      MIN(year) AS year 
+    FROM 
+      Sales 
+    GROUP BY 
+      product_id
+  );
