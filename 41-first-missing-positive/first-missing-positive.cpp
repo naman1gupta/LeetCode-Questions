b4d2679f8d6d097414bd503048732402958057e3
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2024-03-26 10:39:13 +0530

    Time: 43 ms (62.28%) | Memory: 43.8 MB (35.17%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -13,13 +13,13 @@ public:
                 nums[abs(nums[i])-1]*=-1;
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            {
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
                 return i+1;
             }
         }
+
         return n+1;
+    
     }
 };
