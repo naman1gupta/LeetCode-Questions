3a3d55477b6f0b32d2e6f905b34506ed108447a1
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2024-05-05 21:55:36 +0530

    Time: 32 ms (33.33%) | Memory: 13.5 MB (33.33%) - LeetSync

---
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minAnagramLength(string s) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+       int cd=mp[s[0]];
+        for(auto x:mp){
+            cd=__gcd(cd,x.second);
+        }
+        int sum=0;
+        return s.length()/cd;
+    }
+};
