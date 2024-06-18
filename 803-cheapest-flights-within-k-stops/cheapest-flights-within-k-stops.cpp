19bf4ab2e319f58680bcd7635ceee46d0eb3e2f6
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2024-06-18 22:02:58 +0530

    Time: 31 ms (11.53%) | Memory: 19 MB (13.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:   
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-          // create adjacency list
         unordered_map<int,vector<pair<int, int>>> adjList;
         for( auto f : flights )
             adjList[f[0]].push_back( { f[1], f[2] } );
         
-        // minHeap based on cost of distance from source
         priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > minHeap;
-        minHeap.push( { 0, src, 0 } ); // cost, vertex, hops
-        
-        vector<int> dist(n+1, INT_MAX); //to avoid TLE
+        minHeap.push( { 0, src, 0 } ); 
+        vector<int> dist(n+1, INT_MAX); 
         
         while( !minHeap.empty() ) {
             auto t = minHeap.top(); minHeap.pop();
@@ -21,7 +18,6 @@ public:
             if( curr == dst )
                 return cost;
             
-                //Optimization to avoid TLE
             if(dist[curr]<stop) continue;
                  dist[curr]=stop;
 
