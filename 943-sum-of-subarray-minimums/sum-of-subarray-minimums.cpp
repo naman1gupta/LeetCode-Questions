bc9d163b63e014b6bb141dd526dabe98b4d29b84
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2024-06-27 19:04:22 +0530

    Time: 77 ms (22.17%) | Memory: 43.7 MB (85.32%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& a) {
+        if (a.size() == 0)  return 0;
+        int mod = pow(10, 9) + 7;
+        stack<pair <int, int>> s1,s2;
+        int left[a.size()],right[a.size()];
+        for(int i=0;i<a.size();i++){
+            int count=1;
+            while(!s1.empty()&& s1.top().first>a[i]){
+                count+=s1.top().second;
+                s1.pop();
+            }
+            s1.push(make_pair(a[i],count));
+            left[i]=count;
+        }
+        for(int i=a.size()-1;i>=0;i--){
+            int count=1;
+            while(!s2.empty()&& s2.top().first>=a[i]){
+                count+=s2.top().second;
+                s2.pop();
+            }
+            s2.push(make_pair(a[i],count));
+            right[i]=count;
+        }
+        long long ans=0,res=0;
+        for(int i=0;i<a.size();i++){
+            ans=((left[i]*right[i])%mod);
+            ans=(ans * a[i])%mod;
+            res+=ans;
+            res%=mod;
+        }
+        return res;
+    }
+};
