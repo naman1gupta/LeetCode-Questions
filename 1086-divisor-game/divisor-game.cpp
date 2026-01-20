5401157e9f91b5c509cc47b04cb3918d86b0f483
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2026-01-20 11:21:42 +0530

    Time: 3 ms (9.55%) | Memory: 7.9 MB (29.14%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        bool dp[n+10];
+        for(int i=0;i<=n;i++){
+            dp[i]=false;
+        }
+        dp[1]=false;
+        dp[2]=true;
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(((i/j)*j) == i){
+                    if(dp[i-j] == false){
+                        dp[i]=true;
+                        break;
+                    }
+                }
+            }
+
+        }
+        return dp[n];
+    }
+};
