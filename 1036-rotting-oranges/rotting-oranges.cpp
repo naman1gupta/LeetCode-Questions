54ef8cf304236fec36abdf4e717936c42c01237b
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2026-01-27 21:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (73.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        int countfresh = 0;
-        int count=0;
-        int minutes = 0;
-        queue<pair<int,int>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q; //{{row,col},time}
         for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j]==2){
-                    visited[i][j]=2;
-                    q.push({i,j});
-                }else{
-                    visited[i][j]=0;
-                }
-                if(grid[i][j]==1){
-                    countfresh++;
-                }
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2) q.push({{i,j},0});
             }
         }
+
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+
+        int minTime=0;
+
         while(!q.empty()){
-            int arr1[]={-1,0,1,0};
-            int arr2[]={0,1,0,-1};
-            int size = q.size();
-            bool is = false;
-            while(size--){
-            int row = q.front().first;
-            int col = q.front().second;
+
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int time=q.front().second;
+
+            minTime=max(minTime,time);
+
             q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = row + arr1[i];
-                int ncol = col + arr2[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
-                visited[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    visited[nrow][ncol]=2;
-                    count++;
-                    q.push({nrow,ncol});
-                    is = true;
+
+
+            for(int k=0;k<4;k++){
+                int n_r=r+delRow[k];
+                int n_c=c+delCol[k];
+
+
+                if(n_r>=0 && n_r<n && n_c>=0 && n_c<m && grid[n_r][n_c]==1){
+                    grid[n_r][n_c]=2;
+                    q.push({{n_r,n_c},time+1});
                 }
             }
-            
-            }
-            if(is)minutes++;
+
         }
-        if(count!=countfresh)return -1;
-     return minutes;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return minTime;
     }
 };
